831fabf9ec9012dc5db624c7ebd22c9a00fa9184
root <root@Mario>
2024-06-26 22:38:35 +0800

    fix module error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
  
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { AppBar } from "./components/AppBar";
  
 export default function Home() {
   return (
     <>
-    <AppBar/>
     <main className="flex items-center justify-center min-h-screen">
       <div className="border hover:border-slate-900 rounded">
         <WalletMultiButton style={{}} />
