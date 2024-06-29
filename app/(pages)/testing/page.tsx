f51937c10b0d855f4fa2a177a3df140b23b1ca5e
root <root@Mario>
2024-06-29 21:27:29 +0800

    update IPFS feature

--- app/(pages)/testing/page.tsx
+++ app/(pages)/testing/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useRef } from "react";
+import { uploadFile, uploadJSON, fetchJSON } from "../../utils/ipfs";
 
 export default function Home() {
   const [file, setFile] = useState("");
@@ -9,20 +10,15 @@ export default function Home() {
 
   const inputFile = useRef(null);
 
-  const uploadFile = async (fileToUpload) => {
+  const upload = async (fileToUpload) => {
     try {
       setUploading(true);
-      const data = new FormData();
-      data.set("file", fileToUpload);
-      const res = await fetch("/api/ipfs", {
-        method: "POST",
-        body: data,
-      });
-      const resData = await res.json();
-      
-      console.log("Image Cid:", resData.IpfsHash);
-      
+      const resData = await uploadFile(fileToUpload);
       setCid(resData.IpfsHash);
+      
+      console.log("Image Successfully uploaded")
+      console.log(`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${resData.IpfsHash}`)
+      
       setUploading(false);
     } catch (e) {
       console.log(e);
@@ -33,11 +29,25 @@ export default function Home() {
 
   const handleChange = (e) => {
     setFile(e.target.files[0]);
-    uploadFile(e.target.files[0]);
+    upload(e.target.files[0]);
   };
+
+  const addJSON = async ()=>{
+    const sample = {
+        name: "Hello",
+        age: "18"
+    }
+    const resData = await uploadJSON(sample);
+    console.log("JSON Successfully uploaded");
+    console.log(`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${resData.IpfsHash}`);
+
+    const jsonData = await fetchJSON(resData.IpfsHash);
+    console.log(jsonData);
+  }
 
   return (
     <main className="w-full min-h-screen m-auto flex flex-col justify-center items-center">
+      <button onClick={addJSON}>Upload JSON</button>
       <h1>Testing IPFS Feature:</h1>
       Note: You can upload file here, will be used to store data. But don't upload big files or else the memory gets full.
       <input type="file" id="file" ref={inputFile} onChange={handleChange} />
