68e19f7bd74526e844ba5cc1839b68be732d0616
root <root@Mario>
2024-06-29 09:49:45 +0800

    add api section

---
+++ app/(pages)/testing/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { useEffect, useState } from "react";
+ 
+export default function Address() {
+ 
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
+        Testing IPFS Features:  
+    </main>
+  );
+}
