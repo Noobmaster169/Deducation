aa7c761cadd817ad28c1e28e6a4ea9982f10543a
Vincent <vincent.wesley.liem@gmail.com>
2024-07-01 14:13:33 +0800

    create coursecard for dashboard

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,25 @@
+import CourseCard from "../../components/CourseCard";
+import { tempCourses } from "../../data/mockData";
+ 
+export default function Home() {
+  return (
+    <>
+    <div className="flex flex-col gap-4 items-center justify-center h-full">
+      {/* Courses */}
+      <div className="flex flex-col gap-6 bg-blue">
+        <h1 className="font-semibold text-4xl">Courses</h1>
+        <div className="flex gap-5">
+          {tempCourses.slice(0, 3).map(course => (
+            <CourseCard course={course} />
+          ))}
+        </div>
+      </div>
+
+      {/* Bounties */}
+      <div>
+        <h1>Bounties</h1>
+      </div>
+    </div>
+    </>
+  );
+}
