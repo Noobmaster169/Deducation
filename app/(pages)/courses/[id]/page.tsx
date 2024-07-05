e6322436c15ab2a8feee7ad2ae848899cb87f587
Vincent <vincent.wesley.liem@gmail.com>
2024-07-05 17:25:52 +0800

    add text editor

--- app/(pages)/courses/[id]/page.tsx
+++ app/(pages)/courses/[id]/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client"
+
+import React, { LegacyRef, useState } from 'react'
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import "./style.css";
+import TextEditor from '@/components/TextEditor';
 
 const CoursePage = () => {
+  const [value, setValue] = useState<string>("");
   return (
-    <div>CoursePage</div>
-  )
+    <div className="w-full flex text-black">
+      <TextEditor />
+      {/* <textarea
+        className="w-1/2 h-[100vh] p-5 text-lg outline-none border-r-2 border-gray-700 bg-[#252525]"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        className="w-1/2 h-[100vh] p-5 outline-none markdown-body prose text-white"
+      >
+        {value}
+      </Markdown> */}
+    </div>
+  );
 }
 
 export default CoursePage
