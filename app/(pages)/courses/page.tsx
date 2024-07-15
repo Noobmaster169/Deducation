27415d1cf38f15b76e44e9ed619c79f6c0405dbd
Skooyo <davenjtan@gmail.com>
2024-07-15 17:17:21 +0800

    Figuring out error component

--- app/(pages)/courses/page.tsx
+++ app/(pages)/courses/page.tsx
@@ -3,11 +3,10 @@
 import { getAllCourses } from "@/actions/course.actions";
 import CourseCard from "@/components/CourseCard";
 import SearchBar from "@/components/SearchBar";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import NavBar from "@/components/NavBar";
 import PaginationControls from "@/components/PaginationControls";
 import { Suspense } from "react";
-import { useRouter } from "next/router";
 
 const AllCoursePage = () => {
   // const searchParams = useSearchParams() || 1;
