6daedda02829b0757cda52d4ff26f3107e392e65
Vincent <vincent.wesley.liem@gmail.com>
2024-07-15 14:33:40 +0800

    test

---
+++ components/FormItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+type FormItemProps = {
+  placeholder: string;
+  maxLength: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FormItem = ({ placeholder, maxLength, onChange }: FormItemProps) => {
+  return (
+    <>
+      <input
+        className="flex bg-secondary bg-opacity-20 rounded-md p-2 w-[450px] ring-0 focus:outline-none"
+        type="text"
+        placeholder={placeholder}
+        maxLength={maxLength}
+        onChange={onChange}
+      />
+    </>
+  )
+}
+
+export default FormItem
